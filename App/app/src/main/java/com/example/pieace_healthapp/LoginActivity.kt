37e801cf13dc9ae298044029cc3931118e35d108
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-15 19:44:07 +0530

    CompletedLoginLayout

--- App/app/src/main/java/com/example/pieace_healthapp/LoginActivity.kt
+++ App/app/src/main/java/com/example/pieace_healthapp/LoginActivity.kt
@@ -2,11 +2,21 @@ package com.example.pieace_healthapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginbtnClicked(view: View){
+        
+    }
+
+
+    fun loginCreateUserBtnClicked(view:View){
+
     }
 }
