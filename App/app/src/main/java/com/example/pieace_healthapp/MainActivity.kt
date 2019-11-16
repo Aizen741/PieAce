05b2ddbd5586705c29f7eea30fcb74890952ad71
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-17 00:27:57 +0530

    ObjectiveLayout and connected to firebase

--- App/app/src/main/java/com/example/pieace_healthapp/MainActivity.kt
+++ App/app/src/main/java/com/example/pieace_healthapp/MainActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.analytics.FirebaseAnalytics
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
-
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
 
         getStartedBtn.setOnClickListener { val buttonIntent = Intent(this,LoginActivity::class.java)
         startActivity(buttonIntent)}
---
+++ App/app/src/main/java/com/example/pieace_healthapp/secondPage.kt
@@ -0,0 +1,14 @@
+package com.example.pieace_healthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_page)
+
+
+    }
+}
