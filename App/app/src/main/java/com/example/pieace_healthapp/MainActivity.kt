ed8ccc7c44817f66d6da5d84c588a1d33d3bff50
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-28 15:05:33 +0530

    Edited

--- App/app/src/main/java/com/example/pieace_healthapp/MainActivity.kt
+++ App/app/src/main/java/com/example/pieace_healthapp/MainActivity.kt
@@ -3,12 +3,8 @@ package com.example.pieace_healthapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.analytics.FirebaseAnalytics
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
