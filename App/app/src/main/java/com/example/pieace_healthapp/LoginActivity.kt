b27048caee397b61e4c1c9bc1dfa03c92f9c4339
Aizen741 <rahulsivadas2009@gmail.com>
2019-12-04 18:10:34 +0530

    Changes in UI

--- App/app/src/main/java/com/example/pieace_healthapp/LoginActivity.kt
+++ App/app/src/main/java/com/example/pieace_healthapp/LoginActivity.kt
@@ -1,14 +1,19 @@
 package com.example.pieace_healthapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        loginLoginbtn.setOnClickListener {
+            val loginIntent = Intent(this,secondPage::class.java)
+            startActivity(loginIntent)
+        }
 
     }
 
--- App/app/src/main/java/com/example/pieace_healthapp/StartingActivity.kt
+++ App/app/src/main/java/com/example/pieace_healthapp/StartingActivity.kt
@@ -4,19 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_starting.*
 
 
-class MainActivity : AppCompatActivity() {
+class StartingActivity : AppCompatActivity() {
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_starting)
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
 
-        getStartedBtn.setOnClickListener { val buttonIntent = Intent(this,maps_page::class.java)
+        getStartedBtn.setOnClickListener { val buttonIntent = Intent(this,firstPage::class.java)
         startActivity(buttonIntent)}
     }
 
