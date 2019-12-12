a0453ebacef35bd7056e50e94f614320f6ba4bca
Aizen741 <rahulsivadas2009@gmail.com>
2019-12-13 00:36:21 +0530

    additionals

--- App/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/design/R.java
+++ App/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/design/R.java
@@ -1287,15 +1287,15 @@ public final class R {
         public static final int character_counter_pattern = 0x7f0e002f;
         public static final int fab_transformation_scrim_behavior = 0x7f0e0032;
         public static final int fab_transformation_sheet_behavior = 0x7f0e0033;
-        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0e0035;
-        public static final int mtrl_chip_close_icon_content_description = 0x7f0e003a;
-        public static final int password_toggle_content_description = 0x7f0e0042;
-        public static final int path_password_eye = 0x7f0e0043;
-        public static final int path_password_eye_mask_strike_through = 0x7f0e0044;
-        public static final int path_password_eye_mask_visible = 0x7f0e0045;
-        public static final int path_password_strike_through = 0x7f0e0046;
-        public static final int search_menu_title = 0x7f0e0047;
-        public static final int status_bar_notification_info_overflow = 0x7f0e0049;
+        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0e0037;
+        public static final int mtrl_chip_close_icon_content_description = 0x7f0e003d;
+        public static final int password_toggle_content_description = 0x7f0e0046;
+        public static final int path_password_eye = 0x7f0e0047;
+        public static final int path_password_eye_mask_strike_through = 0x7f0e0048;
+        public static final int path_password_eye_mask_visible = 0x7f0e0049;
+        public static final int path_password_strike_through = 0x7f0e004a;
+        public static final int search_menu_title = 0x7f0e004c;
+        public static final int status_bar_notification_info_overflow = 0x7f0e004e;
     }
     public static final class style {
         private style() {}
--- App/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/v7/viewpager/R.java
+++ App/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/v7/viewpager/R.java
@@ -125,7 +125,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0e0049;
+        public static final int status_bar_notification_info_overflow = 0x7f0e004e;
     }
     public static final class style {
         private style() {}
