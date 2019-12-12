1d5c30a71458e9a0338bdef388809792ae83a972
Aizen741 <rahulsivadas2009@gmail.com>
2019-12-12 11:22:36 +0530

    Registration1

--- App/app/src/main/java/com/tiefensuche/motionmate/ui/cards/registration.kt
+++ App/app/src/main/java/com/tiefensuche/motionmate/ui/cards/registration.kt
@@ -2,6 +2,7 @@ package com.tiefensuche.motionmate.ui.cards
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tiefensuche.motionmate.R
 
 class registration : AppCompatActivity() {
 
