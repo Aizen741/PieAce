250c8f60426ac20ce4e12c6b99ea34d6c5fd58bd
Aizen741 <rahulsivadas2009@gmail.com>
2019-12-13 01:29:11 +0530

    additionals3

--- App/app/src/main/java/com/tiefensuche/motionmate/ui/cards/registrationPage.kt
+++ App/app/src/main/java/com/tiefensuche/motionmate/ui/cards/registrationPage.kt
@@ -1,16 +1,27 @@
 package com.tiefensuche.motionmate.ui.cards
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.tiefensuche.motionmate.R
+import com.tiefensuche.motionmate.ui.MainActivity
 
 class registrationPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        .setOnClickListener {
+            startActivity(Intent(this@registrationPage, MainActivity::class.java))
+        }
+
+
+
     }
 
-    fun nextmapbtnClicked(view: View) {}
+
+
+
 }
