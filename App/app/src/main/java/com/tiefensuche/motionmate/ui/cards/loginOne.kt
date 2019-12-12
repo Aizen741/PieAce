70cdb92d3c78240574485ade283bd6232031c065
Aizen741 <rahulsivadas2009@gmail.com>
2019-12-12 10:02:19 +0530

    NewLogin

---
+++ App/app/src/main/java/com/tiefensuche/motionmate/ui/cards/loginOne.kt
@@ -0,0 +1,13 @@
+package com.tiefensuche.motionmate.ui.cards
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tiefensuche.motionmate.R
+
+class loginOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_one)
+    }
+}
